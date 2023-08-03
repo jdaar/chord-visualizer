2ba308bc3698b09f786a232c3d832b8c017e487f
Jhonatan David <jhonatandaar@gmail.com>
2023-08-03 11:14:36 -0500

    fix: custom domain so no base path

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,7 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		paths: {
-			base: dev ? '' : process.env.BASE_PATH
+			base: dev ? '' : ''
 		},
 		adapter: adapter({
 			pages: 'build',
