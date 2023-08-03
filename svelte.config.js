d3a10e3da50ae7a6cc0279c852e238a82ddf7472
Jhonatan David <jhonatandaar@gmail.com>
2023-08-03 11:24:33 -0500

    fix: custom domain so custom paths

--- svelte.config.js
+++ svelte.config.js
@@ -11,6 +11,10 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		paths: {
+			base: '',
+			assets: 'https://chord.jasprilla.me' 
+		},
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
