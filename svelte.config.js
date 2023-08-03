6e7e224fe811eca29dbb4abe6c44541cc4be796d
Jhonatan David <jhonatandaar@gmail.com>
2023-08-03 09:42:03 -0500

    fix: prettier styles

--- svelte.config.js
+++ svelte.config.js
@@ -14,8 +14,8 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		paths: {
-            base: dev ? '' : process.env.BASE_PATH,
-        },
+			base: dev ? '' : process.env.BASE_PATH
+		},
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
