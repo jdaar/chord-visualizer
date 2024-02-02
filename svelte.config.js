5ae2140c2b1546e5f2dc17602c653b612712b4a3
Jhonatan David <jhonatandaar@gmail.com>
2024-02-02 12:06:49 -0500

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		paths: {
 			base: '',
-			assets: 'https://chord.jhonatan.tech'
+			assets: 'https://chords.jhonatan.tech'
 		},
 		adapter: adapter({
 			pages: 'build',
